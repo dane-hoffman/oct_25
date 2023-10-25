8f9fd1ec48fc534bc4f4cfc3a816159c76eece57
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-10-25 10:05:03 -0600

    oct_25 workflow; petting zoo

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-//create four functions, that use two numbers; to add, mult, sub, divide
+// //create four functions, that use two numbers; to add, mult, sub, divide
 
-//Create function the takes in two numbers as an argument
-const addTwoNumbers = (num1, num2) => {
-    //use the "+" operator within the parameters of the function
-    return num1 + num2;
-};
+// //Create function the takes in two numbers as an argument
+// const addTwoNumbers = (num1, num2) => {
+//     //use the "+" operator within the parameters of the function
+//     return num1 + num2;
+// };
 
-//Create function the takes in two numbers as an argument
-const multiplyTwoNumbers = (num1, num2) => {
-    //use the "*" operator within the parameters of the function
-    return num1 * num2;
-};
+// //Create function the takes in two numbers as an argument
+// const multiplyTwoNumbers = (num1, num2) => {
+//     //use the "*" operator within the parameters of the function
+//     return num1 * num2;
+// };
 
-//Create function the takes in two numbers as an argument
-const subtractTwoNumbers = (num1, num2) => {
-    //use the "-" operator within the parameters of the function
-    return num1 - num2;
-};
+// //Create function the takes in two numbers as an argument
+// const subtractTwoNumbers = (num1, num2) => {
+//     //use the "-" operator within the parameters of the function
+//     return num1 - num2;
+// };
 
-//Create function the takes in two numbers as an argument
-const divideTwoNumbers = (num1, num2) => {
-    //use the "/" operator within the parameters of the function
-    return num1 / num2;
-};
+// //Create function the takes in two numbers as an argument
+// const divideTwoNumbers = (num1, num2) => {
+//     //use the "/" operator within the parameters of the function
+//     return num1 / num2;
+// };
 
-//Display the solution to each of the functions.
-//Test each as created
+// //Display the solution to each of the functions.
+// //Test each as created
+
+//------------------------------------
+
+//Create a funciton called `animalSounds`
+
+//return the following values vased on the animal (string) that is passed in
+//cow = moo
+//dog = woof
+//cat = meow
+//alligator = snap snap
+
+//Otherwise, display `we don't have that animal in the petting zoo.`
